7d93d99e65ee0760d61c3ebe5da6de9d662b87ee
Seshi Reddy <seshredy@gmail.com>
2019-09-02 14:41:05 +0100

    add new book

--- src/main/java/com/seshi/authorbookdetails/AuthorBookController.java
+++ src/main/java/com/seshi/authorbookdetails/AuthorBookController.java
@@ -14,8 +14,9 @@ public class AuthorBookController {
         AuthorBooks a1 = new AuthorBooks(1,"Spring Fundamentals",20);
         AuthorBooks a2 = new AuthorBooks(2, "Spring Boot",30);
         AuthorBooks a3 = new AuthorBooks(3, "Spring Rest Apress",40);
+        AuthorBooks a4 = new AuthorBooks(3, "Spring Rest Apress",40);
 
-        return Arrays.asList(a1, a2, a3);
+        return Arrays.asList(a1, a2, a3,a4);
 
     }
 
